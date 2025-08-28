0f17b20354fe8dd6ab62de52aa335e5fee9c92ee
ikasaidi <ikramsaidi47@gmail.com>
2025-08-28 13:38:11 -0400

    first commit

---
+++ src/main/java/ikasaidi/backend_lab/BackendLabApplication.java
@@ -0,0 +1,13 @@
+package ikasaidi.backend_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ikasaidi/backend_lab/BackendLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ikasaidi.backend_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
